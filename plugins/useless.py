cb2f439513f5c0abd50aafb2496fa750625c47f5
TD green library <talaridinesh2005@gmail.com>
2026-01-01 16:41:44 +0530

    Update comments in useless.py with new credits

--- plugins/useless.py
+++ plugins/useless.py
@@ -25,7 +25,9 @@ async def useless(_,message: Message):
 
 
 
-# Tech freak 
+# TD Bot Dev
 # Don't Remove Credit!!!
-# Telegram Channel @Tech_freak_tamil
-# Developer @devilo7
+#YouTube channel link https://youtube.com/@tdbotdev?si=j3HVY4E69-C4ZDcw
+# Telegram Channel @TD_Public_Bots
+# Developer @TD_Public_Bots
+
