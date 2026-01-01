5fe12ef196a98cd8bda55c78d467cf5699569f92
TD green library <talaridinesh2005@gmail.com>
2026-01-01 16:06:53 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -11,7 +11,8 @@ async def web_server():
 
 
 
-# Tech freak 
+# @TD_Public_Bots
 # Don't Remove Credit!!!
-# Telegram Channel @Team_TD_Links
-# Developer @Team_TD_Links
+#YouTube channel link https://youtube.com/@tdbotdev?si=j3HVY4E69-C4ZDcw
+# Telegram Channel @TD_Public_Bots
+# Developer @TD_Public_Bots
